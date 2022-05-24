e309231d2a0db3658efa11a6770fbcc4488cdee4
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-24 16:24:20 +0200

    text

--- index.php
+++ index.php
@@ -64,11 +64,19 @@ $faqs = [
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
   <title>Google Faq</title>
 </head>
 <body>
   
-  <p> </p>
+  <div class="container d-flex justify-content-center">
+
+  <?php 
+        
+            ?>
+
+  </div>
   
 </body>
 </html>
